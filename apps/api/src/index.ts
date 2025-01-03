a59a3d120ba37b58af5dddc086b53413f1b771e3
Nicholas Bekeris <nicholas@bekeris.me>
2025-01-03 16:19:02 -0600

    fix: Considerations for api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,18 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get('/', () => 'Hi Elysia')
+  .get('/id/:id', ({ params: { id } }) => id)
+  .post('/mirror', ({ body }) => body, {
+      body: t.Object({
+          id: t.Number(),
+          name: t.String()
+      })
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
 
-export default app.fetch;
+export type App = typeof app 
