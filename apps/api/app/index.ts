23f84690a879649c13c487768abc688f8c6ab932
Nicholas Bekeris <nicholas@bekeris.me>
2025-01-03 13:17:02 -0600

    Full reset

--- apps/api/app/index.ts
+++
@@ -1,7 +0,0 @@
-import { Elysia } from 'elysia';
-
-const app = new Elysia();
-
-app.get("/", () => "Hello, Elysia!");
-
-export default app;
--- apps/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { Elysia } from 'elysia';
-
-const app = new Elysia();
-
-app.get("/", () => "Hello, Elysia!");
-
-app.listen(3000).then(() => {
-  console.log("Server started on http://localhost:3000");
-});
