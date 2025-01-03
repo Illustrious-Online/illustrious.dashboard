a35b6e78a59f281fbf430acb8d4b9a3d7f866621
Nicholas Bekeris <nicholas@bekeris.me>
2025-01-03 02:01:58 -0600

    chore: Convert from Bun.env to process.env

--- apps/api.illustrious.cloud/src/config/index.ts
+++ apps/api.illustrious.cloud/src/config/index.ts
@@ -1,33 +1,33 @@
 import data from "../../package.json";
 
-const isTestEnvironment = Bun.env.NODE_ENV === "test";
+const isTestEnvironment = process.env.NODE_ENV === "test";
 
 export default {
   app: {
-    env: Bun.env.NODE_ENV ?? 'development',
-    url: Bun.env.APP_URL,
+    env: process.env.NODE_ENV ?? 'development',
+    url: process.env.APP_URL,
     name: data.name,
     version: data.version,
-    host: Bun.env.TEST_APP_HOST || Bun.env.APP_HOST || "localhost",
+    host: process.env.TEST_APP_HOST || process.env.APP_HOST || "localhost",
     port:
-      (isTestEnvironment ? Bun.env.TEST_APP_PORT : Bun.env.APP_PORT) || "8000",
-    dashboardUrl: Bun.env.DASHBOARD_URL,
-    sentryUrl: Bun.env.SENTRY_URL,
+      (isTestEnvironment ? process.env.TEST_APP_PORT : process.env.APP_PORT) || "8000",
+    dashboardUrl: process.env.DASHBOARD_URL,
+    sentryUrl: process.env.SENTRY_URL,
   },
   auth: {
-    url: Bun.env.AUTH0_URL || "localhost",
-    audience: Bun.env.AUTH0_AUD || "illustrious",
-    clientId: Bun.env.CLIENT_ID || "lorem",
-    clientSecret: Bun.env.CLIENT_SECRET || "ipsum",
-    supabaseId: Bun.env.SUPABASE_ID || "sit",
-    edgeKey: Bun.env.EDGE_KEY!,
-    supabaseServiceRoleKey: Bun.env.SUPABASE_SERVICE_ROLE_KEY!,
+    url: process.env.AUTH0_URL || "localhost",
+    audience: process.env.AUTH0_AUD || "illustrious",
+    clientId: process.env.CLIENT_ID || "lorem",
+    clientSecret: process.env.CLIENT_SECRET || "ipsum",
+    supabaseId: process.env.SUPABASE_ID || "sit",
+    edgeKey: process.env.EDGE_KEY!,
+    supabaseServiceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY!,
   },
   db: {
-    dbName: Bun.env.DB_NAME!,
-    dbPassword: Bun.env.DB_PASSWORD!,
-    dbUsername: Bun.env.DB_USERNAME!,
-    dbPort: Bun.env.DB_PORT!,
-    dbHost: Bun.env.DB_HOST!,
+    dbName: process.env.DB_NAME!,
+    dbPassword: process.env.DB_PASSWORD!,
+    dbUsername: process.env.DB_USERNAME!,
+    dbPort: process.env.DB_PORT!,
+    dbHost: process.env.DB_HOST!,
   },
 };
